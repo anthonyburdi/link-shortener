15a617d3778d6a2253fd29a8dcfdff484f740ba9
anthonyburdi <anthonyburdi@gmail.com>
2015-04-23 16:27:30 -0400

    updated provision to use /vagrant directory and not git clone

--- provision.sh
+++ provision.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
-# apt-get update
-# apt-get upgrade -y
+apt-get update
+apt-get upgrade -y
 
-apt-get install -y nano
 apt-get install -y git
 
 apt-get -y install python-pip
+
 pip install virtualenv
 
 # git clone https://anthonyburdi@bitbucket.org/anthonyburdi/link-shortener.git
-cd link-shortener
+cd /vagrant
 virtualenv venv
 source venv/bin/activate
 pip install -r /vagrant/requirements.txt
+
+echo "cd /vagrant" >> $HOME/.bashrc
